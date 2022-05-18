edabd562602563b66cc260b066986025538dbc3f
Alexis2879 <https://alexis2879.github.io/trabajo-salv/>
2022-05-17 22:52:08 -0500

    prueba

--- semana04/dia4/web/admin.py
+++ semana04/dia4/web/admin.py
@@ -1,10 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Categoria,Producto,Cliente,Pedido,PedidoDetalle
+from .models import Categoria,Producto
 
 admin.site.register(Categoria)
 admin.site.register(Producto)
-admin.site.register(Cliente)
-admin.site.register(Pedido)
-admin.site.register(PedidoDetalle)
